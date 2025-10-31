a151036323479dfe3f92f0c3c2f167699eac2b89
LOAN Ngo <mrs.loan1999@gmail.com>
2025-10-31 22:58:03 +0700

    Updated: Content for matching with event

--- react-app/src/components/ClippedDrawer/index.js
+++ react-app/src/components/ClippedDrawer/index.js
@@ -55,7 +55,7 @@ export default function ClippedDrawer() {
         >
           <Toolbar>
             <Typography variant="h6" noWrap>
-              Fancy Store
+              Demo Store
             </Typography>
           </Toolbar>
         </AppBar>
